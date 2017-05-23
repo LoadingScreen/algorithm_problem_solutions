8ee54a277a6b23a5ef5f19d491dde5bdb7143ca9
LoadingScreen <kgallessich@gmail.com>
2017-05-23 14:08:52 -0700

    dried up membership test

--- keyboard_row.rb
+++ keyboard_row.rb
@@ -4,15 +4,15 @@ def find_words(words)
     ltop = ["q","w","e","r","t","y","u","i","o","p"]
     lmid = ["a","s","d","f","g","h","j","k","l"]
     lbott = ["z","x","c","v","b","n","m"]
-    
+    ll = [ltop, lmid, lbott]
+
     output = []
     
     words.each do |word|
-        membership = [0,0,0] #first thing that came to mind, don't laugh
+        membership = [0,0,0]
         word.each_char do |char|
-            membership[0] = 1 if (ltop.include?(char))
-            membership[1] = 1 if (lmid.include?(char)) #not dry so sorry
-            membership[2] = 1 if (lbott.include?(char))
+            3.times do |i|
+                membership[i] = 1 if ll[i].include?(char)
         end
         output << word if (membership[0] + membership[1] + membership[2] == 1)
     end
